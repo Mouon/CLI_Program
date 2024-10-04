4257d4ff591386bf125c156bd585532c8e995324
HyunJun Mun <137624597+Mouon@users.noreply.github.com>
2024-10-04 10:05:35 +0900

    트

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("안녕하세요 전기프2팀 입니다. 테스트용");
+        System.out.println("안녕하세요 전기프2팀 입니다. 테스트");
     }
 }
